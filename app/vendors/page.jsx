10d60d8596d0ee860af279b03c307f1d30ab49d0
Ksenia <baranova-ksenia1990@gmail.com>
2024-02-21 13:13:23 +0200

    added dummy data

--- app/vendors/page.jsx
+++ app/vendors/page.jsx
@@ -1,5 +1,21 @@
+import VendorCard from "@/components/VendorCard"
+import vendors from "../../dummydata.json"
 const VendorsPage = () => {
-	return <div>VendorsPage</div>
+	return (
+		<section className="px-4 py-6">
+			<div className="container-xl lg:container m-auto px-4 py-6">
+				{vendors.length === 0 ? (
+					<p>No vendors found.</p>
+				) : (
+					<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+						{vendors.map(vendor => (
+							<VendorCard key={vendor._id} vendor={vendor} />
+						))}
+					</div>
+				)}
+			</div>
+		</section>
+	)
 }
 
 export default VendorsPage
