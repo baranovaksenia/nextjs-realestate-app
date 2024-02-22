a6c4a90609ae97ae6f019ba083979699ea61744a
Ksenia <baranova-ksenia1990@gmail.com>
2024-02-22 21:50:27 +0200

    create loading and error pages

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero"
 import HomeVendors from "@/components/HomeVendors"
 import InfoBoxes from "@/components/InfoBoxes"
+import { TextRevealCard } from "@/components/ui/text-reveal-card"
 
 export default function Home() {
 	return (
@@ -8,6 +9,10 @@ export default function Home() {
 			<Hero />
 			<InfoBoxes />
 			<HomeVendors />
+			<TextRevealCard
+				text="Explore Crypto Services"
+				revealText="Explore Crypto Services"
+			/>
 		</>
 	)
 }
